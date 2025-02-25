daf4cb51a99cf43d1087a00a51c3ee0c03b6a1ee
eaglethrost <dimazanugrah12@gmail.com>
2025-02-25 13:09:32 +1100

    parse all sheets

--- parser.py
+++ parser.py
@@ -3,12 +3,20 @@ import xml.etree.ElementTree as ET
 
 def excel_to_xml(file_name):
     wb = openpyxl.load_workbook(file_name)
-    sheet_name = "1 User Report"
-    sheet = wb[sheet_name]
-    root = ET.Element("root")
-    for row in sheet.rows:
-        for cell in row:
-            ET.SubElement(root, "cell", value=str(cell.value))
+    root = ET.Element("workbook")
+
+    for wb_sheet in wb.worksheets:
+        sheet_name = wb_sheet.title
+        sheet_root = ET.Element("worksheet", {"name": sheet_name}) 
+        sheet = wb[sheet_name]
+        for row in sheet.rows:
+            row_root = ET.Element("row")
+            for excel_cell in row:
+                cell = ET.SubElement(row_root, "cell")
+                cell.text = str(excel_cell.value)
+            sheet_root.append(row_root)
+        root.append(sheet_root)
+        
     tree = ET.ElementTree(root)
     tree.write("xml/input.xml")
         
